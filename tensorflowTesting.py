74946f3637bed82ea615ea84501c76615eeddc09
s <devansh.subnani05@gmail.com>
2020-09-26 14:16:23 +0530

    first commit

---
+++ tensorflowTesting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import tensorflow as tf
+from keras.models import load_model
+import cv2
+import numpy as np
+
+
+# In[ ]:
+
+
+classes=[0,1,2,3,4,5,6,7,8,9]
+
